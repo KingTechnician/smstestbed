a3a945722ddd3ecf74aad46ff3772644addd414c
Isaiah Freeman <104329626+KingTechnician@users.noreply.github.com>
2024-02-27 16:16:00 -0500

    Correcting an Expo compilation issue. No actual changes - disregard.

--- proxy/test.js
+++ proxy/test.js
@@ -27,3 +27,61 @@ fetch(url)
     }).catch(error=>{
         console.error(error)
     })
+
+
+/*const fetchData = async()=>
+  {
+    console.log("I'm over here.")
+    await fetch("http://localhost:5000/data")
+    .then(response=>response.text())
+    .then(str=>
+      {
+        var searchResults = []
+        var parser  =new DOMParser()
+        var xmlDocument = parser.parseFromString(str.replaceAll("\n",""),"application/xml")
+        var htmlString = new XMLSerializer().serializeToString(xmlDocument)
+        var deviceStreams = xmlDocument.getElementsByTagName("DeviceStream")
+        
+        for(const d of deviceStreams)
+        {
+
+          const name = d.getAttribute("name")
+          const uuid = d.getAttribute("uuid")
+          var components = []
+          const componentStreams = d.getElementsByTagName("ComponentStream")
+          for (const c of componentStreams)
+          {
+            const component = c.getAttribute("component")
+            const id = c.getAttribute("componentId")
+            const tables = omitChildNodesByTagName(c,"text")
+            for(const t of tables)
+            {
+              var dataDictionary = {}
+              const tagName = t.tagName || "none"
+              if(tagName!=="none")
+              {
+                console.log(tagName)
+                dataDictionary[tagName] = []
+                var dataPoints = t.querySelectorAll("*")
+                for(const dp of dataPoints)
+                {
+                  dataEntry = {
+                    "dataItemId":dp.getAttribute("dataItemId"),
+                    "timestamp":dp.getAttribute("timestamp"),
+                    "name": dp.getAttribute("name"),
+                    "sequence":dp.getAttribute("sequence"),
+                    "subType":dp.getAttribute("subType"),
+                  }
+                }
+                console.log(dataDictionary)
+              }
+            }
+            const componentEntry = {
+              "component":component,
+              "id":id
+            }
+          }
+        }
+      })
+  }
+  */
