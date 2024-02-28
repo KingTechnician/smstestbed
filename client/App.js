fca4f286c7f68e1d8b4a7a31a7489ae226c245f5
Isaiah Freeman <104329626+KingTechnician@users.noreply.github.com>
2024-02-27 20:27:49 -0500

    Starting the display for the frontend

--- client/App.js
+++ client/App.js
@@ -6,6 +6,135 @@ import {createTheme,ThemeProvider,styled} from '@mui/material/styles'
 import { AntDesign } from '@expo/vector-icons';
 
 
+const dataColumns = [
+  {id:"description",label:"Description",minWidth:170},
+  {id:"dataItemId",label:"DataItemId",minWidth:100},
+  {id:"timestamp",label:"Timestamp",minWidth:170},
+  {id:"name",label:"Name",minWidth:170},
+  {id:"sequence",label:"Sequence",minWidth:170},
+  {id:"subType",label:"SubType",minWidth:170},
+  {id:"value",label:"Value",minWidth:170}
+]
+
+
+const columns = [
+  { id: 'name', label: 'ame', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+];
+
+function tableData(description,dataItemId,timestamp,name,sequence,subType,value)
+{
+  return {description,dataItemId,timestamp,name,sequence,subType,value}
+
+}
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
+export function ColumnGroupingTable(props) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [dataRows,setDataRows] = React.useState([])
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  return (
+    <Paper sx={{ width: '100%' }}>
+      <TableContainer sx={{ maxHeight: 440,backgroundColor:"#303030" }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {dataColumns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ top: 0, minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
 export const themeOptions = {
   palette: {
     mode:'dark',
@@ -30,19 +159,25 @@ export const themeOptions = {
 
 export default function App() {
   const [search,setSearch] = useState("")
+  const [devices,setDevices] = useState([])
   const [type,setType] = useState(10)
   const theme = createTheme(themeOptions)
   const displayStyler = createTheme({root:{"& .MuiTableCell-head":{color:"white",backgroundColor:"blue"}}})
   const handleTypeChange = (event) =>{
+    console.log(devices)
     setType(event.target.value)
+  }
+  
+  const fetchDevices = async() =>
+  {
+    await fetch("http://localhost:5000/data")
+    .then(response=>response.text())
+    .then(str=>{setDevices(JSON.parse(str))})
   }
   React.useEffect(()=>
   {
-    console.log("I'm over here.")
-    fetch("http://localhost:5000/data")
-    .then(response=>response.text())
-    .then(str=>console.log(str))
-
+    fetchDevices()
+    console.log(devices)
   },[])
   return (
     <ThemeProvider theme={theme}>
@@ -83,6 +218,40 @@ export default function App() {
           </Grid>
           </FormControl>
           <br></br>
+          {devices.map((device)=>
+          {
+            return(
+            <Accordion sx={{backgroundColor:"424242"}}>
+              <AccordionSummary
+                expandIcon={<AntDesign name="caretup" size={24} color="black" />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography variant="h3">{device.name+", uuid: "+device.uuid}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Grid container spacing={2}>
+                  {device.components.map((component)=>
+                  {
+                    return(
+                    <Grid item xs={6}>
+                      <Typography variant="h4">{component.component+" , "+component.id}</Typography>
+                      {
+                        Object.keys(component.data).map((key)=>
+                        {
+                          return(
+                            <ColumnGroupingTable data={component.data[key]}/>
+                          )
+                        })
+                      }
+                    </Grid>
+                    )
+                  })}
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
+            )
+          })}
           <Accordion sx={{backgroundColor:"#424242",color:"white"}}>
             <AccordionSummary
               expandIcon={<AntDesign name="caretup" size={24} color="black" />}
--- proxy/proxy.js
+++ proxy/proxy.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 5000;
+const jsdom = require("jsdom")
 const xmldom = require('xmldom')
 
 
@@ -25,24 +26,74 @@ app.get('/data',(req,res)=>
     res.setHeader('Access-Control-Allow-Origin', '*');
     const url = "https://smstestbed.nist.gov/vds/sample";
 
-fetch(url)
-.then(response =>{
-
-    if(!response.ok)
-    {
-        throw new Error("Network response was not okay");
-    }
-    return response.text()
-})
-.then(str =>
-    {
-        var parser = new xmldom.DOMParser()
-
-        console.log(str)
-        res.send(str)
-    }).catch(error=>{
-        console.error(error)
+    fetch(url)
+    .then(response =>{
+    
+        if(!response.ok)
+        {
+            throw new Error("Network response was not okay");
+        }
+        return response.text()
     })
+    .then(str =>
+        {
+            const dom = new jsdom.JSDOM(str)
+            var devices = []
+            const deviceStreams = dom.window.document.getElementsByTagName("DeviceStream") //DeviceStream tags reference individual devices
+            for (const d of deviceStreams)
+            {
+              const name = d.getAttribute("name")
+              const uuid = d.getAttribute("uuid")
+              var components = []
+              const componentStreams = d.getElementsByTagName("ComponentStream") //ComponentStream tags reference individual componeents
+              for(const c of componentStreams)
+              {
+                var dataDictionary = {}
+                const component = c.getAttribute("component")
+                const id = c.getAttribute("componentId")
+                const tables = c.childNodes
+                for(const t of tables)
+                {
+                  const tagName = t.tagName || "none" //Some tags are actually empty lines, this will filter them out
+                  if(tagName!=="none") //We can assume anything in this if statement is a valid tag
+                  {
+                    const dataPoints = t.querySelectorAll("*") //Grab regardless of name
+                    dataDictionary[tagName] = []
+                    for( const dp of dataPoints)
+                    {
+                      dataEntry = {
+                        "description":dp.tagName, //This gives more detail than the smstestbed site - shows the actual purpose of the data point
+                        "dataItemId":dp.getAttribute("dataItemId"),
+                        "timestamp":dp.getAttribute("timestamp"),
+                        "name": dp.getAttribute("name"),
+                        "sequence":dp.getAttribute("sequence"),
+                        "subType":dp.getAttribute("subType"),
+                        "value":dp.textContent
+                      }
+                      dataDictionary[tagName].push(dataEntry)// Add by tag name to specify type of data points
+                    
+                    }
+                  }
+                }
+                const componentEntry = {
+                  "component":component,
+                  "id":id,
+                  "data":dataDictionary
+                }
+                components.push(componentEntry) // Add component to list of components
+              }
+              const device = {
+                name:name,
+                uuid:uuid,
+                components:components
+              }
+              devices.push(device) //Add device to list of devices
+            }
+            res.send(JSON.stringify(devices))
+            //console.log(xmlDocument)
+        }).catch(error=>{
+            console.error(error)
+        })    
 })
 
 
